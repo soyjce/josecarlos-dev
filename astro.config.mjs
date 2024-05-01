f87971230146df23a6d78ba7f51bf969290ecc14
soyjce <joseca.hdez@gmail.com>
2024-05-01 16:47:56 -0600

    prueba astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-
-import robotsTxt from "astro-robots-txt"
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://josecarlos.dev'
-})
+export default defineConfig({});
