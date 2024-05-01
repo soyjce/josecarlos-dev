5f979add77f81125a1fc272597afa87e271293b8
soyjce <joseca.hdez@gmail.com>
2024-05-01 15:31:14 -0600

    agregamos deply

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://soyjce.github.io/josecarlos-dev'
+  site: 'https://soyjce.github.io'
 })
